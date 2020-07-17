802a63ae068876bac2ab2a2ac38b77b6b4c18954
Yanko Ivanov <y.ivanov@codecoda.com>
2020-07-17 07:41:33 +0300

    - Fix unit tests

--- server/__test__/utils.test.js
+++ server/__test__/utils.test.js
@@ -1,14 +1,20 @@
 const utils = require('../src/utils');
 
 describe('db return ohm', () => {
-    test('returns Ohm object', () => {
-        expect(utils.getOhmById('1').toBeDefined());
+    test('returns Ohm object', async () => {
+        expect((await utils.getOhmById('1'))).toBeDefined();
     });
 
-    test('has a valid history', () => {
-        const ohm = utils.getOhmById('1');
+    test('has a valid history', async () => {
+        const ohm = await utils.getOhmById('0');
         const statuses = [ 'CREATED', 'PREPARING', 'READY', 'IN_DELIVERY','DELIVERED', 'REFUSED']
-        const isValidStatus = statuses.includes(ohm.history[0].state)
+        const isValidStatus = statuses.some(status => status == ohm.history[0].state)
         expect(isValidStatus).toBe(true);
+    });
+
+    test('has an updated status', async () => {
+        utils.updateOhmStatus('1e62adfe', 'DELIVERED');
+        const ohm = await utils.getOhmByTrackingId('1e62adfe');
+        expect(ohm.status).toBe('DELIVERED');
     });
 })
