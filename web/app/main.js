3c5496be5edcf6d11819cdf213b310f023178291
Yanko Ivanov <y.ivanov@codecoda.com>
2020-07-17 06:11:13 +0300

    - Add gitginore for node_modules - Add main functionality to the controller - Misc commit to angular's package.json.

--- web/app/main.js
+++ web/app/main.js
@@ -1,12 +1,38 @@
 angular
     .module("ohm-delivery", [])
-    .controller("tracking", function($scope, $http) {
-        $scope.sendData = function() {
-            $http.get(`/ohms/${this.trackingId}`)
-            .then((error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            }, (result) => {
-                this.errorMessage = '';
-            });
+    .controller("tracking", function ($scope, $http, statusService) {
+
+        $scope.showHistory = false;
+
+        const successHandler = (result) => {
+            $scope.errorMessage = '';
+            this.ohm = result.data;
+            $scope.ohm = result.data;
+
+            $scope.statusLabel = statusService.getLabel(result.data.status);
+        }
+
+        const errorHandler = (error) => {
+            $scope.errorMessage = error.data;
+        }
+
+        $scope.markReceived = function() {
+            $http.get(`/ohms/forward/${this.trackingId}/1`)
+                .then(successHandler, errorHandler);
+        }
+
+        $scope.markReject = function() {
+            $http.get(`/ohms/forward/${this.trackingId}/2`)
+                .then(successHandler, errorHandler);
+        }
+
+        $scope.sendData = function () {
+            if(! this.trackingId ) {
+                $scope.errorMessage = 'Please enter a valid tracking code.';
+                return;
+            }
+
+            $http.get(`/ohms/tracking/${this.trackingId}`)
+                .then(successHandler, errorHandler);
         };
     });
